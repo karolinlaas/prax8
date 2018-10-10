ba252724bab6a090e214df98574c42a54aedae5a
Karolin Laas <karolin.laas@khk.ee>
2018-10-10 16:26:32 +0300

    Lisatud on inkrement.

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
 let numInput = document.querySelector('#numInput');
 let factOutput = document.querySelector('#factOutput');
+let incInput = document.querySelector('#incInput');
 
 const getFact = () => {
+    factOutput.innerHTML = '';
     let number = numInput.value;
+    let inc = incInput.value;
+    let baseUrl = 'http://numbersapi.com/';
+    // Vaja saada sarnane URL
+    //http://numbersapi.com/1..3
+    let url = `${baseUrl}${number}..${parseInt(number) + parseInt(inc)}`;
         
-    if (number !='')
-        axios.get('http://numbersapi.com/'+number)
+    if (number !='' && inc !='') {
+    axios.get(url)
         .then(response => {
-            factOutput.innerText = response.data;
+            for (let item in response.data) {
+                let li = document.createElement('li');
+                li.innerText = response.data[item];
+                factOutput.appendChild(li);
+            }
         })
         .catch(error => {
             console.log(error);
         })
-    
+    }
 }
 
 numInput.addEventListener('input', getFact);
+incInput.addEventListener('input', getFact);
                              
